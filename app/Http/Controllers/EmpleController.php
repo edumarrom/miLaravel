c96286fc8234508e472dc02c581b83a2573facff
Eduardo Martínez <emartinezr161@outlook.com>
2021-11-23 14:51:37 +0100

    metodo show emple, pero no hay vista

--- app/Http/Controllers/EmpleController.php
+++ app/Http/Controllers/EmpleController.php
@@ -9,11 +9,34 @@ class EmpleController extends Controller
 {
     public function index()
     {
-        $emples = DB::select('SELECT * FROM emple e
-                        LEFT JOIN depart d
-                               ON e.depart_id = d.id ');
+        $empleados = DB::select('SELECT e.*, d.denominacion
+                                FROM emple e
+                                JOIN depart d
+                                  ON depart_id = d.id');
         return view('emple.index', [
-            'empleados' => $emples,
+            'empleados' => $empleados,
         ]);
+    }
+
+    public function show($id)
+    {
+        $empleado = $this->findEmpleado($id);
+
+        return view('emple.show', [
+            'empleado' => $empleado,
+        ]);
+    }
+
+    private function findEmpleado($id)
+    {
+        $empleado = DB::select('SELECT e.*, d.denominacion
+                                  FROM emple e
+                                  JOIN depart d
+                                    ON depart_id = d.id
+                                 WHERE e.id = ?', [$id]);
+
+        abort_unless($empleado, 404);
+
+        return $empleado[0];
     }
 }
--- resources/views/emple/index.blade.php
+++ resources/views/emple/index.blade.php
@@ -30,7 +30,9 @@
                         <tr class="whitespace-nowrap">
                             <td class="px-6 py-4">
                                 <div class="text-sm text-gray-900">
-                                    {{$emples->nombre}}
+                                    <a href="/emple/{{ $emples->id }}" class="text-blue-800 hover:underline">
+                                        {{ $emples->nombre }}
+                                    </a>
                                 </div>
                             </td>
                             <td class="px-6 py-4">
